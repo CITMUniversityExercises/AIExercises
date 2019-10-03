601b532a9f0667e3d2d88fc10b404023673a28b3
Aitor Simona Bouzas <aulazsimonaaitor@gmail.com>
2019-10-03 14:39:44 +0200

    uploading part 2

---
+++ Steering behaviours/Assets/Steering/SteeringFlee.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SteeringFlee : MonoBehaviour {
+
+	Move move;
+
+	// Use this for initialization
+	void Start () {
+		move = GetComponent<Move>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Steer(move.target.transform.position);
+	}
+
+	public void Steer(Vector3 target)
+	{
+        // TODO 2: Same as Steering seek but opposite direction
+        Vector3 velocity = ((move.target.transform.position - transform.position) * move.max_mov_acceleration);
+
+        move.AccelerateMovement(-velocity);
+    }
+}
---
+++ Steering behaviours/Assets/Steering/SteeringSeek.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SteeringSeek : MonoBehaviour {
+
+	Move move;
+
+	// Use this for initialization
+	void Start ()
+    {
+		move = GetComponent<Move>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+       
+
+        Steer(move.target.transform.position);
+	}
+
+	public void Steer(Vector3 target)
+	{
+        if (move == null)
+        {
+            move = GetComponent<Move>();
+        }
+
+        // TODO 1: accelerate towards our target at max_acceleration
+        // use move.AccelerateMovement()
+        Vector3 velocity = ((target - transform.position) * move.max_mov_acceleration);
+
+        move.AccelerateMovement(velocity);
+
+    }
+}
