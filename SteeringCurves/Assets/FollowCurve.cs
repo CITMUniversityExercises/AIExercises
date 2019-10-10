f473126a2b85372a07bf710bdb83f623f3e9e4dd
Aitor Simona Bouzas <aulazsimonaaitor@gmail.com>
2019-10-10 14:37:14 +0200

    uploading new steering behaviours

---
+++ SteeringCurves/Assets/FollowCurve.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BansheeGz.BGSpline.Components;
+using BansheeGz.BGSpline.Curve;
+
+public class FollowCurve : MonoBehaviour
+{
+    public BGCcMath curve;
+    float ratio = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        // desired_initialpos = math.CalcPositionByClosestPoint(transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ratio += (0.1f * Time.deltaTime);
+
+        if (ratio > 1)
+            ratio = 0;
+
+        Vector3 final_position = curve.CalcPositionByDistanceRatio(ratio);
+
+        transform.position = final_position;
+    }
+}
---
+++ SteeringCurves/Assets/Steering/SteeringFollowPath.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using BansheeGz.BGSpline.Components;
+using BansheeGz.BGSpline.Curve;
+
+public class SteeringFollowPath : MonoBehaviour {
+
+	Move move;
+	SteeringArrive arrive;
+    public BGCcMath curve;
+
+    public float ratio_increment = 0.1f;
+    public float min_distance = 3.0f;
+    float current_ratio = 0.0f;
+
+    Vector3 closest_point;
+
+
+	// Use this for initialization
+	void Start () {
+		move = GetComponent<Move>();
+		arrive = GetComponent<SteeringArrive>();
+
+        // TODO 1: Calculate the closest point from the tank to the curve
+        closest_point = curve.CalcPositionByClosestPoint(move.transform.position, out current_ratio);
+
+        ratio_increment = move.max_mov_speed / curve.GetDistance();
+
+        current_ratio /= curve.GetDistance();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        // TODO 2: Check if the tank is close enough to the desired point
+        // If so, create a new point further ahead in the path
+
+        Vector3 distance_to_target = closest_point - move.transform.position;
+        //distance_to_target.Normalize();
+
+
+        if (distance_to_target.magnitude < min_distance)
+        {
+            current_ratio += ratio_increment;
+            closest_point = curve.CalcPositionByDistanceRatio(current_ratio);
+        }
+        if (current_ratio > 1)
+            current_ratio = 0;
+
+        arrive.Steer(closest_point);
+
+	}
+
+	void OnDrawGizmosSelected() 
+	{
+
+		if(isActiveAndEnabled)
+		{
+			// Display the explosion radius when selected
+			Gizmos.color = Color.green;
+			// Useful if you draw a sphere were on the closest point to the path
+		}
+        Gizmos.DrawSphere(closest_point, 3);
+	}
+}
