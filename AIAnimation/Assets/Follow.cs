14362c65d7b9b1cd62758fde31025de77d6d3d30
Aitor Simona Bouzas <simonaaitor@gmail.com>
2019-10-24 14:37:31 +0200

    uploading animation handout

---
+++ AIAnimation/Assets/Follow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Follow : MonoBehaviour
+{
+    Animator animator;
+    NavMeshAgent agent;
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(agent.remainingDistance > 2.0f)
+        {
+            animator.SetBool("movement", true);
+        }
+        else
+            animator.SetBool("movement", false);
+
+        agent.SetDestination(target.transform.position);
+    }
+}
